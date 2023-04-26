9eea2df7cd70307358a2c10102df98bc52f39751
Mateusz Myrcik <mateuszmyrcik@MacBook-Pro-Mateusz.local>
2023-04-26 21:10:45 +0200

    feat: base colors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,20 +8,58 @@ import { store } from "@/app/store";
 import { Provider } from "react-redux";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import clsx from "clsx";
+import { createTheme, ThemeProvider } from "@mui/material";
 
 const roboto = Roboto({
   subsets: ["latin"],
   weight: ["100", "400", "500", "700"],
 });
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#E3F2FD",
+      main: "#2196F3",
+      dark: "#1565C0",
+      contrastText: "#FFFFFF",
+    },
+    secondary: {
+      light: "#EDE7F6",
+      main: "#673AB7",
+      dark: "#4527A0",
+      contrastText: "#FFFFFF",
+    },
+    success: {
+      light: "#B9F6CA",
+      main: "#69F0AE",
+      dark: "#00C853",
+    },
+    error: {
+      light: "#EF9A9A",
+      main: "#F44336",
+      dark: "#C62828",
+    },
+    warning: {
+      light: "#B9F6CA",
+      main: "#FFC107",
+      dark: "#FFC107",
+    },
+  },
+});
+
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const containerClasses = clsx(roboto.className, "min-h-screen bg-gradient");
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <div className={roboto.className}>
-        <Provider store={store}>
-          <Component {...pageProps} />
-        </Provider>
-      </div>
+      <ThemeProvider theme={theme}>
+        <div className={containerClasses}>
+          <div className=""></div>
+          <Provider store={store}>
+            <Component {...pageProps} />
+          </Provider>
+        </div>
+      </ThemeProvider>
     </LocalizationProvider>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,32 @@ module.exports = {
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
+ theme: {
+    extend: {
+      backgroundColor: {
+        'primary': '#5DA1D5',
+        'secondary': '#F0F0F0',
+        'accent': '#FFA726',
+        'nav': '#1E3A5F',
+        'input': '#F0F0F0',
+      },
+      textColor: {
+        'primary': '#5DA1D5',
+        'secondary': '#F0F0F0',
+        'accent': '#FFA726',
+        'dark': '#333333',
+        'placeholder': '#A9A9A9',
+        'label': '#1E3A5F',
+      },
+      borderColor: {
+        'input': '#D3D3D3',
+      },
+    },
+  },
+    variants: {
+    backgroundColor: ['responsive', 'hover', 'focus'],
+    borderColor: ['responsive', 'hover', 'focus'],
+    textColor: ['responsive', 'hover', 'focus'],
   },
   plugins: [],
 };
