b17c38195616ad4131416c2e15683e400964ce78
Mateusz Myrcik <mateuszmyrcik@MacBook-Pro-Mateusz.local>
2023-05-27 16:05:42 +0200

    feat: base expenses table styling

--- src/views/expenses/index.tsx
+++ src/views/expenses/index.tsx
@@ -1,10 +1,13 @@
 import { PrimaryTemplate } from "@/app/layout";
 import { RootState, store } from "@/app/store";
+import { MovingOutlined } from "@mui/icons-material";
 
 import { Button } from "@mui/material";
+import clsx from "clsx";
 
 import { useRouter } from "next/router";
 import { Provider, useSelector } from "react-redux";
+import { formatDate, getTotalAmount, isValuePositive } from "./lib";
 
 export const ExpensesView = () => {
   const { expenses } = useSelector((state: RootState) => state.expensesSlice);
@@ -14,33 +17,61 @@ export const ExpensesView = () => {
     <>
       <Provider store={store}>
         <PrimaryTemplate>
-          <div className="w-10/12 mx-auto mt-4">
-            <h1>Family budget | Expenses </h1>
+          <div className="rounded-lg  bg-white w-full">
+            <div className="p-6 border-b">
+              <h1 className="text-lg font-medium">Zarządzaj wydatkami</h1>
+            </div>
 
-            <Button
-              onClick={() => {
-                push("/expense");
-              }}
-            >
-              Dodaj nowy wydatek
-            </Button>
+            <div className="p-6 space-y-4">
+              <div className="flex justify-between">
+                <div
+                  className={clsx(
+                    "text-base",
+                    isValuePositive(getTotalAmount(expenses))
+                      ? "text-success-main"
+                      : "text-error-main"
+                  )}
+                >
+                  {getTotalAmount(expenses)}
+                  <MovingOutlined></MovingOutlined>
+                </div>
+                <Button
+                  onClick={() => {
+                    push("/expense");
+                  }}
+                  variant="contained"
+                >
+                  Dodaj nową transakcję
+                </Button>
+              </div>
 
-            <ul className="rounded-md">
-              {expenses.map((expense, index) => (
-                <li key={index} className="rounded-md border-gray-100 border-4">
-                  <div className="flex flex-row justify-between">
-                    <span className="text-2xl font-bold">{expense.name}</span>
-                    <span className="text-2xl font-bold">
-                      {expense.amount.value}
-                    </span>
+              <div className="flex flex-col gap-2 text-sm">
+                {expenses.map(({ amount, date, name, category }, index) => (
+                  <div
+                    key={index}
+                    className="grid grid-cols-[40px_300px_100px_100px] items-center p-2 border-[1px] rounded-lg gap-2 hover:bg-gray-100 cursor-pointer justify-between"
+                  >
+                    <div className=" text-gray-500 text-sm">
+                      {formatDate(date)}
+                    </div>
+                    <div className="basis-40 flex-grow-0">{name}</div>
+                    <div className="inline-flex items-center justify-center rounded-full bg-emerald-100 px-2.5 py-0.5 text-emerald-700">
+                      {category}
+                    </div>
+                    <div
+                      className={clsx(
+                        "text-base",
+                        isValuePositive(amount.value)
+                          ? "text-success-main"
+                          : "text-error-main"
+                      )}
+                    >
+                      {amount.value}
+                    </div>
                   </div>
-                  <span className="text-2xl font-bold">
-                    {String(expense.date)}
-                  </span>
-                  <span className="text-lg font-bold">{expense.category}</span>
-                </li>
-              ))}
-            </ul>
+                ))}
+              </div>
+            </div>
           </div>
         </PrimaryTemplate>
       </Provider>
--- src/views/expenses/lib.ts
+++ src/views/expenses/lib.ts
@@ -1,5 +1,26 @@
 import { Expense } from "@/shared/domain/types/shared";
 
+// TODO: move to shared lib
+export const formatDate = (date: Date) => {
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+
+  const formattedDay = day < 10 ? `0${day}` : `${day}`;
+  const formattedMonth = month < 10 ? `0${month}` : `${month}`;
+
+  return `${formattedDay}.${formattedMonth}`;
+};
+
+
+export const isValuePositive = (value: number) => value > 0;
+
+export const getTotalAmount = (expenses: Expense[]) => {
+  return expenses.reduce((acc, expense) => {
+    return acc + expense.amount.value;
+  }, 0);
+};
+
+// Generated, if not needed, delete
 export const getExpenseCategories = (expenses: Expense[]) => {
   return expenses.reduce((acc, expense) => {
     if (acc.includes(expense.category)) {
